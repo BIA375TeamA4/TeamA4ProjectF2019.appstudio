607e7b9e6c4cbac6f5c4069fd17d5d46295817d7
Connor Hock <cbh06002@creighton.edu>
2019-11-20 23:38:04 -0600

    huge

--- forms/createNewAccount1/createNewAccount1.js
+++ forms/createNewAccount1/createNewAccount1.js
@@ -9,7 +9,7 @@ btnSubmitInformation.onclick=function(){
     let createPassword = inptCreatePassword.value
     let verifyPassword = inptCreatePassword.value
     let blankValue = ""
-    let query2 = "SELECT * FROM user WHERE username = " + '"' + createUsername + '"' + " AND password = " + '"' + createPassword + '"'
+    let query2 = "SELECT * FROM user WHERE username = " + '"' + createUsername + '"'
     let query3 = "INSERT into user (first_name, last_name, email, city, state, username, password) values (" + "'"+ createFirstName + "', '" + createLastName + "', '" + createEmail + "', '" + createCity + "', '" + createState + "', '" + createUsername + "', '" + createPassword + "'" + ");"
     
   if (createUsername != blankValue && createPassword != blankValue && verifyPassword != blankValue && createPassword == verifyPassword) {
@@ -18,26 +18,25 @@ btnSubmitInformation.onclick=function(){
               results = JSON.parse(req1.responseText)
               console.log(results)
               if (results.length == 0) {
-                    req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + query3)
-                    if (req2.status == 200) {
-                          req3 =  Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + query2)
-                           if (req1.status == 200) { 
-                                  results2 = JSON.parse(req3.responseText)
-                                  console.log(results2)
-                           } else 
-                     }
-                }   else 
-                    lblSubmitAllInformation1.value = "This username or password "
-        } else
-          lblSubmitAllInformation1.value = "Please submit all information and ensure proper verification of password."
-}
-  
-}
-
-
+                  req2 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + query3)
+                  if (req2.status == 200) {
+                        req3 =  Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + query2)
+                        if (req1.status == 200) { 
+                              results2 = JSON.parse(req3.responseText)
+                              console.log(results2)
+                        }
+                  } else
+                    lblSubmitAllInformation1.value = "Please submit all information "
+              } else
+                lblSubmitAllInformation1.value = "This username is already taken."
+          } else
+            lblSubmitAllInformation1.value = "Please submit all information and ensure proper verification of password."
+      } else
+        lblSubmitAllInformation1.value = "Please submit all information and ensure proper verification of password."
+  }
       
       
-      
+                     
       
       
       
