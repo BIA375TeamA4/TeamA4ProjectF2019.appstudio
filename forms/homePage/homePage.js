d210d700854ba7c8c94aa92102e906689339b194
Tom Murphy <tommurphy@creighton.edu>
2019-11-24 21:56:10 -0600

    11.24 before zoom

--- forms/homePage/homePage.js
+++ forms/homePage/homePage.js
@@ -1,5 +1,21 @@
 let currentUser = localStorage.getItem("username");
 
+<<<<<<< Updated upstream
+=======
+homePage.onshow=function(){
+    let queryItem1 = "SELECT user_id FROM user WHERE username = " + '"' + currentUser + '"'
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + queryItem1)
+    userID = JSON.parse(req1.responseText)
+    console.log(userID)
+    localStorage.setItem("userID", userID)
+    txtWelcome.value = "Welcome, " + currentUser + "!"
+}
+
+let currentUserID = localStorage.getItem("userID")
+
+
+/* STILL NEED MORE CONTENT */
+>>>>>>> Stashed changes
 
 
 btnMap.onclick=function(){
--- forms/profilePage/profilePage.js
+++ forms/profilePage/profilePage.js
@@ -1,3 +1,17 @@
+<<<<<<< Updated upstream
+=======
+profilePage.onshow=function(){
+    let queryItem5 = "SELECT first_name, last_name, username, city, state FROM user WHERE user_id = " + '"' + currentUserID + '"'
+    req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=tpm62822&pass=Beta#118&database=375groupa4&query=" + queryItem5)
+    if (req1.status == 200) { 
+       results = JSON.parse(req1.responseText)
+       console.log(results)
+       localStorage.setItem("userInfo", results)
+    }     
+}
+
+var userInfo = localStorage.getItem("userInfo")
+>>>>>>> Stashed changes
 
 
 
