208140cdc5a6bd9fe57a38d6d5aa6a50ddfe9a0f
Tom Murphy <tommurphy@creighton.edu>
2019-11-25 12:27:14 -0600

    update map

--- forms/map/map.js
+++ forms/map/map.js
@@ -1,4 +1,4 @@
-
+/* POTENTIAL CODE FOR MAP, WE ARE NOT FINISHED WITH THIS AND COULD USE SUGGESTIONS*/
 function myMap() {
 var mapProp = {
     center:new google.maps.LatLng(51.508742,-0.120850),
@@ -14,7 +14,7 @@ src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"
 
 var map_parameters = { center: {lat: 47.490, lng: -117.585}, zoom: 8 };
 var map = new google.maps.Map(document.getElementById('map'), map_parameters);
-
+*/
 
 
 
