7048547a8bcd3d3168ed97cfd1038083d701fad7
Conor Baker <cab24313@creighton.edu>
2019-12-04 17:55:54 -0600

    new

--- forms/map/map.js
+++ forms/map/map.js
@@ -1,37 +1,36 @@
-/* POTENTIAL CODE FOR MAP, WE ARE NOT FINISHED WITH THIS AND COULD USE SUGGESTIONS 
-
-function myMap() {
-var mapProp = {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-
-API Key = AIzaSyA3cAfXGoImw3OexYi9dhEvkbGhbZxf_vM
-
-
-src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY"
-
-var map_parameters = { center: {lat: 47.490, lng: -117.585}, zoom: 8 };
-var map = new google.maps.Map(document.getElementById('map'), map_parameters); */
+ //For more documentation on the options, see
+ //https://developers.google.com/maps/documentation/javascript/reference?csw=1
+ var marker;
+ var infowindow;
 
 
 
+ function gotLocation(location, lat, long) {
+     GoogleMap1.mapOptions.latitude = location.coords.latitude;
+     GoogleMap1.mapOptions.longitude = location.coords.longitude;
+     GoogleMap1.refresh();
 
+     //Put a marker on our location
+     point = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
+     marker = GoogleMap1.setMarker({
+         position: point
+     });
+     //Add an infoWindow to our marker
+     infowindow = GoogleMap1.setInfoWindow("Lat: " + location.coords.latitude + " Lng: " + location.coords.longitude, marker);
+     NSB.WaitCursor(false);
+ }
+ 
+btnLocation.onclick = function() {
+     navigator.geolocation.getCurrentPosition(gotLocation);
+     NSB.WaitCursor(true);
+ };
 
-btnHome1.onclick=function(){
-  ChangeForm(homePage)
-}
+ btnHideMarker.onclick = function() {
+     if (typeof(marker) != "object") {
+         return;
+     };
+     infowindow = undefined;
+     marker.setMap(null);
+     GoogleMap1.refresh();
+ };
 
-btnMyList1.onclick=function(){
-  ChangeForm(myList)
-}
-
-btnEntry1.onclick=function(){
-  ChangeForm(entries)
-}
-
-btnProfile1.onclick=function(){
-  ChangeForm(profilePage)
-}
